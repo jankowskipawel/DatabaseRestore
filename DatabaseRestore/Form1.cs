521eb56025b77c15f738471b69ef9e33de839912
jankowskipawel <39927846+jankowskipawel@users.noreply.github.com>
2021-07-19 18:47:57 +0200

    Add database restoring, exception handling, path checking

--- DatabaseRestore/Form1.cs
+++ DatabaseRestore/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,36 +32,97 @@ namespace DatabaseRestore
             string username = loginTextBox.Text;
             string password = passwordTextBox.Text;
             string databaseName = databaseNameTextBox.Text;
+            string backupPath = backupPathTextBox.Text;
+            string portStr = portTextBox.Text;
             string connString = $"Host={host};Username={username};Password={password};Database={databaseName}";
-            await using var conn = new NpgsqlConnection(connString);
-            await conn.OpenAsync();
-            //get tables
-            using (var cmd = new NpgsqlCommand("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name; ; ", conn))
-            using (  var reader = cmd.ExecuteReader())
+            try
             {
-                reader.Read();
-                if (reader.HasRows)
+                var port=0;
+                Int32.TryParse(portStr, out port);
+                CheckFilePath(backupPath);
+                await using var conn = new NpgsqlConnection(connString);
+                await conn.OpenAsync();
+                //get tables
+                using (var cmd = new NpgsqlCommand("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name; ; ", conn))
+                using (var reader = cmd.ExecuteReader())
                 {
-                    //check if database exists, if exists ask if continue or not
-                    DialogResult dialogResult = MessageBox.Show("Database is not empty. Do you want to continue?", "Database restore", MessageBoxButtons.YesNo);
-                    if (dialogResult == DialogResult.No)
+                    reader.Read();
+                    if (reader.HasRows)
                     {
-                        return;
+                        //check if database exists, if exists ask if continue or not
+                        DialogResult dialogResult = MessageBox.Show("Database is not empty. Do you want to continue?", "Database restore", MessageBoxButtons.YesNo);
+                        if (dialogResult == DialogResult.No)
+                        {
+                            return;
+                        }
                     }
                 }
+                //remove schema public cascade
+                using (var cmd1 = new NpgsqlCommand("DROP SCHEMA public CASCADE", conn))
+                {
+                    cmd1.ExecuteNonQuery();
+                }
+                //add schema public
+                using (var cmd1 = new NpgsqlCommand("CREATE SCHEMA public", conn))
+                {
+                    cmd1.ExecuteNonQuery();
+                }
+                List<string> commands = new List<string>(2);
+                string cmdText = $"pg_restore -U {username} -h {host} -p {port} -d {databaseName} -1 --disable-triggers {backupPath}";
+                commands.Add($"set \"PGPASSWORD={password}\"");
+                commands.Add(cmdText);
+                RunCommands(commands);
+                //ADD postgres/bin to PATH
             }
-            //remove schema public cascade
-            using (var cmd1 = new NpgsqlCommand("DROP SCHEMA public CASCADE", conn))
+            catch (Exception exception)
             {
-                cmd1.ExecuteNonQuery();
+                MessageBox.Show(exception.Message);
             }
-            //add schema public
-            using (var cmd1 = new NpgsqlCommand("CREATE SCHEMA public", conn))
+        }
+
+        static void RunCommands(List<string> cmds, string workingDirectory = "")
+        {
+            var process = new Process();
+            var psi = new ProcessStartInfo();
+            psi.FileName = "cmd.exe";
+            psi.RedirectStandardInput = true;
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+            psi.UseShellExecute = false;
+            psi.WorkingDirectory = workingDirectory;
+            process.StartInfo = psi;
+            process.Start();
+            process.OutputDataReceived += (sender, e) => { Console.WriteLine(e.Data); };
+            process.ErrorDataReceived += (sender, e) => { Console.WriteLine(e.Data); };
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            using (StreamWriter sw = process.StandardInput)
             {
-                cmd1.ExecuteNonQuery();
+                foreach (var cmd in cmds)
+                {
+                    sw.WriteLine(cmd);
+                }
             }
-            //pg_restore -U postgres -h localhost -p 5432 -d baba -W -1 --disable-triggers E:\Downloads\test.backup
-            //ADD postgres/bin to PATH
+            process.WaitForExit();
+        }
+
+        public static void CheckFilePath(string path)
+        {
+            if (path.Length == 0)
+            {
+                throw new ArgumentException("File path cannot be empty");
+            }
+
+            if (!File.Exists(path) || !IsPathValid(path) || !path.EndsWith(".backup"))
+            {
+                throw new ArgumentException($"{path} is not a valid file");
+            }
+        }
+
+        public static bool IsPathValid(string filePath)
+        {
+            HashSet<char> invalidCharacters = new HashSet<char>(Path.GetInvalidPathChars());
+            return !string.IsNullOrEmpty(filePath) && !filePath.Any(pc => invalidCharacters.Contains(pc));
         }
     }
 }
