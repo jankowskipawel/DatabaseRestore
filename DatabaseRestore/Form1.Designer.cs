563460f4aeaf0794ebec2221c3dc4d645ed1e3ea
jankowskipawel <39927846+jankowskipawel@users.noreply.github.com>
2021-07-19 15:11:53 +0200

    Add datasbase connection and schema clearing

--- DatabaseRestore/Form1.Designer.cs
+++ DatabaseRestore/Form1.Designer.cs
@@ -29,26 +29,30 @@ namespace DatabaseRestore
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ipTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.backupPathTextBox = new System.Windows.Forms.TextBox();
+            this.backupPathButton = new System.Windows.Forms.Button();
+            this.restoreDatabaseButton = new System.Windows.Forms.Button();
+            this.backupFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.databaseNameTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // ipTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(25, 35);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(240, 23);
-            this.textBox1.TabIndex = 0;
+            this.ipTextBox.Location = new System.Drawing.Point(25, 35);
+            this.ipTextBox.Name = "ipTextBox";
+            this.ipTextBox.Size = new System.Drawing.Size(240, 23);
+            this.ipTextBox.TabIndex = 0;
+            this.ipTextBox.Text = "localhost";
             // 
             // label1
             // 
@@ -89,77 +93,104 @@ namespace DatabaseRestore
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(25, 120);
+            this.label5.Location = new System.Drawing.Point(25, 160);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(117, 15);
             this.label5.TabIndex = 5;
             this.label5.Text = ".backup file location:";
             // 
-            // textBox2
+            // portTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(271, 35);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(35, 23);
-            this.textBox2.TabIndex = 6;
-            this.textBox2.Text = "5432";
+            this.portTextBox.Location = new System.Drawing.Point(271, 35);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(35, 23);
+            this.portTextBox.TabIndex = 6;
+            this.portTextBox.Text = "5432";
             // 
-            // textBox3
+            // loginTextBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(25, 84);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(138, 23);
-            this.textBox3.TabIndex = 7;
+            this.loginTextBox.Location = new System.Drawing.Point(25, 84);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(138, 23);
+            this.loginTextBox.TabIndex = 7;
+            this.loginTextBox.Text = "postgres";
             // 
-            // textBox4
+            // passwordTextBox
             // 
-            this.textBox4.Location = new System.Drawing.Point(169, 83);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.PasswordChar = '*';
-            this.textBox4.Size = new System.Drawing.Size(134, 23);
-            this.textBox4.TabIndex = 8;
+            this.passwordTextBox.Location = new System.Drawing.Point(169, 83);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(134, 23);
+            this.passwordTextBox.TabIndex = 8;
+            this.passwordTextBox.Text = "Test123!@#";
             // 
-            // textBox5
+            // backupPathTextBox
             // 
-            this.textBox5.Location = new System.Drawing.Point(25, 138);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(249, 23);
-            this.textBox5.TabIndex = 9;
+            this.backupPathTextBox.Location = new System.Drawing.Point(25, 178);
+            this.backupPathTextBox.Name = "backupPathTextBox";
+            this.backupPathTextBox.Size = new System.Drawing.Size(249, 23);
+            this.backupPathTextBox.TabIndex = 9;
             // 
-            // button1
+            // backupPathButton
             // 
-            this.button1.Location = new System.Drawing.Point(281, 138);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(25, 23);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "...";
-            this.button1.UseVisualStyleBackColor = true;
+            this.backupPathButton.Location = new System.Drawing.Point(281, 178);
+            this.backupPathButton.Name = "backupPathButton";
+            this.backupPathButton.Size = new System.Drawing.Size(25, 23);
+            this.backupPathButton.TabIndex = 10;
+            this.backupPathButton.Text = "...";
+            this.backupPathButton.UseVisualStyleBackColor = true;
+            this.backupPathButton.Click += new System.EventHandler(this.backupPathButton_Click);
             // 
-            // button2
+            // restoreDatabaseButton
             // 
-            this.button2.Location = new System.Drawing.Point(102, 181);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(127, 23);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Restore database";
-            this.button2.UseVisualStyleBackColor = true;
+            this.restoreDatabaseButton.Location = new System.Drawing.Point(102, 221);
+            this.restoreDatabaseButton.Name = "restoreDatabaseButton";
+            this.restoreDatabaseButton.Size = new System.Drawing.Size(127, 23);
+            this.restoreDatabaseButton.TabIndex = 11;
+            this.restoreDatabaseButton.Text = "Restore database";
+            this.restoreDatabaseButton.UseVisualStyleBackColor = true;
+            this.restoreDatabaseButton.Click += new System.EventHandler(this.restoreDatabaseButton_Click);
+            // 
+            // backupFileDialog
+            // 
+            this.backupFileDialog.FileName = "openFileDialog1";
+            // 
+            // databaseNameTextBox
+            // 
+            this.databaseNameTextBox.Location = new System.Drawing.Point(25, 131);
+            this.databaseNameTextBox.Name = "databaseNameTextBox";
+            this.databaseNameTextBox.Size = new System.Drawing.Size(278, 23);
+            this.databaseNameTextBox.TabIndex = 12;
+            this.databaseNameTextBox.Text = "postgres";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Database name:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(328, 221);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(328, 256);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.databaseNameTextBox);
+            this.Controls.Add(this.restoreDatabaseButton);
+            this.Controls.Add(this.backupPathButton);
+            this.Controls.Add(this.backupPathTextBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.portTextBox);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ipTextBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -169,18 +200,21 @@ namespace DatabaseRestore
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox ipTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox backupPathTextBox;
+        private System.Windows.Forms.Button backupPathButton;
+        private System.Windows.Forms.Button restoreDatabaseButton;
+        private System.Windows.Forms.OpenFileDialog backupFileDialog;
+        private System.Windows.Forms.TextBox databaseNameTextBox;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- DatabaseRestore/Form1.cs
+++ DatabaseRestore/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace DatabaseRestore
 {
@@ -17,5 +18,48 @@ namespace DatabaseRestore
             InitializeComponent();
         }
 
+        private void backupPathButton_Click(object sender, EventArgs e)
+        {
+            backupFileDialog.ShowDialog();
+            backupPathTextBox.Text = backupFileDialog.FileName;
+        }
+
+        private async void restoreDatabaseButton_Click(object sender, EventArgs e)
+        {
+            string host = ipTextBox.Text;
+            string username = loginTextBox.Text;
+            string password = passwordTextBox.Text;
+            string databaseName = databaseNameTextBox.Text;
+            string connString = $"Host={host};Username={username};Password={password};Database={databaseName}";
+            await using var conn = new NpgsqlConnection(connString);
+            await conn.OpenAsync();
+            //get tables
+            using (var cmd = new NpgsqlCommand("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name; ; ", conn))
+            using (  var reader = cmd.ExecuteReader())
+            {
+                reader.Read();
+                if (reader.HasRows)
+                {
+                    //check if database exists, if exists ask if continue or not
+                    DialogResult dialogResult = MessageBox.Show("Database is not empty. Do you want to continue?", "Database restore", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.No)
+                    {
+                        return;
+                    }
+                }
+            }
+            //remove schema public cascade
+            using (var cmd1 = new NpgsqlCommand("DROP SCHEMA public CASCADE", conn))
+            {
+                cmd1.ExecuteNonQuery();
+            }
+            //add schema public
+            using (var cmd1 = new NpgsqlCommand("CREATE SCHEMA public", conn))
+            {
+                cmd1.ExecuteNonQuery();
+            }
+            //pg_restore -U postgres -h localhost -p 5432 -d baba -W -1 --disable-triggers E:\Downloads\test.backup
+            //ADD postgres/bin to PATH
+        }
     }
 }
